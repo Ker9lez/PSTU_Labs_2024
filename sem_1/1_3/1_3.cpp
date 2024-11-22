9982e2701114b6397a3334ecec4f6942b4f295b4
Ker9lez <r_n_a_07@mail.ru>
2024-11-23 01:03:54 +0500

    files was added
    turing&markov - 1_switch

---
+++ sem_1/1_3/1_3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a, b;
+    cout << "Введите число: \n";
+    cin >> a;
+    if (a < 5)
+    {
+        b = a * 3;
+    }
+    else if (a > 5 and a < 7)
+    {
+        b = a / 10;
+    }
+    else
+    {
+        b = a + 3;
+    }
+    cout << b << endl;
+    return 0;
+}
---
+++ sem_1/1_switch/1_switch.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float a, b, ans;
+    int code;
+    cout << "Введите 2 числа, по одному в каждой строке: \n";
+    cin >> a >> b;
+    cout << "Введите код операции (1 - '+', 2 - '-', 3 - '*', 4 - '/'): \n";
+    cin >> code;
+    switch (code)
+    {
+        case 1: ans = a + b; cout << ans << endl; break;
+        case 2: ans = a - b; cout << ans << endl; break;
+        case 3: ans = a * b; cout << ans << endl; break;
+        case 4:
+        {
+            if (b == 0)
+            {
+                cout << "Деление на 0 невозможно" << endl;
+                break;
+            }
+            else
+            {
+                ans = a / b;
+                cout << ans << endl;
+                break;
+            }
+        }
+    default: cout << "Неверный код операции \n";
+    }
+    return 0;
+}
