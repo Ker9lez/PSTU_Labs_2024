3e4d295c9036b72d2a4cf69eff86a8f96ff567ff
Ker9lez <r_n_a_07@mail.ru>
2024-12-04 16:13:32 +0500

    2.10

---
+++ sem_1/2.10/2.10.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, a, max;
+	cout << "Введите длину последовательности: \n";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "Введите следующее число : \n";
+		cin >> a;
+		if (i == 1) { max = a; }
+		else if (a > max) { max = a; }
+	}
+	cout << "Максимальный элемент равен - " << max << endl;
+	return 0;
+}
