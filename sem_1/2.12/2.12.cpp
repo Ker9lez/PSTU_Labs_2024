88e5635416da05d886b18cfc125badfd87e3fd08
Ker9lez <r_n_a_07@mail.ru>
2024-12-15 23:03:55 +0500

    2.12

---
+++ sem_1/2.12/2.12.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, tmp, max, min;
+	cout << "Введите длину последовательности: \n";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "Введите следующее число: \n";
+		cin >> tmp;
+		if (i == 1)
+		{
+			min = tmp;
+			max = tmp;
+		}
+		else
+		{
+			if (tmp > max)
+			{
+				max = tmp;
+			}
+			else if (tmp < min)
+			{
+				min = tmp;
+			}
+		}
+	}
+	cout << "Сумма максимального и минимального чисел равна - " << max + min << endl;
+	return 0;
+}
