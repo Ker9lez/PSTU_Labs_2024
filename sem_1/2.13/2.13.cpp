44b880c3afb6c2d0e7bb646adc64623fe3f7a6b7
Ker9lez <r_n_a_07@mail.ru>
2024-12-15 23:30:49 +0500

    2.13

---
+++ sem_1/2.13/2.13.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, c, D, x1, x2;
+	cout << "Введите коэффициенты уравнения: \n";
+	cin >> a >> b >> c;
+	D = pow(b, 2) - 4 * a * c;
+	if (D >= 0)
+	{
+		x1 = (-1 * b + sqrt(D)) / (2 * a);
+		x2 = (-1 * b - sqrt(D)) / (2 * a);
+		cout << "Корни уравнения равны - " << x1 << ", " << x2 << endl;
+	}
+	else
+	{
+		cout << "Корней нет" << endl;
+	}
+	return 0;
+}
