0d41d626b9ca254dc581040d400382b4fbf1d5be
Ker9lez <r_n_a_07@mail.ru>
2024-12-21 18:15:48 +0500

    2.14

---
+++ sem_1/2.14/2.14.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	int sum = 0;
+	cout << "Введите число: \n";
+	cin >> n;
+	while (n > 0)
+	{
+		sum += n % 10;
+		n /= 10;
+	}
+	cout << "Сумма цифр числа равна - " << sum << endl;
+	return 0;
+}
