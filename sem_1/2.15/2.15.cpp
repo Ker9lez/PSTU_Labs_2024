069a565ac3b3fa7b973ab1a2d3660c67ab234aae
Ker9lez <r_n_a_07@mail.ru>
2024-12-21 18:10:36 +0500

    2.15

---
+++ sem_1/2.15/2.15.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, s, k;
+	bool flag = false;
+	cout << "Введите число и цифру: \n";
+	cin >> n >> s;
+	while (n > 0 && !flag)
+	{
+		k = n % 10;
+		if (k == s)
+		{
+			flag = true;
+			cout << "Цифра есть в числе" << endl;
+		}
+		else { n /= 10; }
+	}
+	if (!flag) { cout << "Цифры нет в числе" << endl; }
+	return 0;
+}
