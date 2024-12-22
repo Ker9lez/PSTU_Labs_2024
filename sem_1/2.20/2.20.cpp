a561c9cedc10404fc8fa65e3e5e8321f23f671e5
Ker9lez <r_n_a_07@mail.ru>
2024-12-22 22:55:24 +0500

    2.20

---
+++ sem_1/2.20/2.20.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float n, s, a;
+	int i = 1;
+	bool flag = false;
+	cout << "Введите 2 числа: \n";
+	cin >> n >> s;
+	while (i <= n && !flag)
+	{
+		a = sin(n + i / n);
+		if (a == s) { flag = true; }
+		else { i++; }
+	}
+	if (flag)
+	{
+		cout << "Элемент найден" << endl;
+	}
+	else
+	{
+		cout << "Элемент не найден" << endl;
+	}
+	return 0;
+}
