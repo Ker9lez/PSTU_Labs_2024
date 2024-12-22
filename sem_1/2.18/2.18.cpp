06eb094230010f1679ce104b583e49abb7b50471
Ker9lez <r_n_a_07@mail.ru>
2024-12-22 22:42:32 +0500

    2.18

---
+++ sem_1/2.18/2.18.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	int r = 0;
+	cout << "Введите число: \n";
+	cin >> n;
+	while (n > 0)
+	{
+		r = r * 10 + n % 10;
+		n /= 10;
+	}
+	cout << r << endl;
+	return 0;
+}
