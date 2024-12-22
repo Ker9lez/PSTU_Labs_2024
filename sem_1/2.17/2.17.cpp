bf40e5163808a72fa7a6c24fb44e2087dadeee41
Ker9lez <r_n_a_07@mail.ru>
2024-12-22 22:37:39 +0500

    2.17

---
+++ sem_1/2.17/2.17.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	float x, fact, sum;
+	cout << "Введите 2 числа: \n";
+	cin >> n >> x;
+	sum = 1 + x;
+	fact = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		fact *= i;
+		sum += (pow(x, i) / fact);
+	}
+	cout << sum << endl;
+	return 0;
+}
