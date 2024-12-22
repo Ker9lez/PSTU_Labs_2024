b5ec0bb45e96bbb2a90b5ae2b2b0423b04130a32
Ker9lez <r_n_a_07@mail.ru>
2024-12-22 23:01:39 +0500

    2.20, 2.21

---
+++ sem_1/2.21/2.21.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int tmp, next;
+	bool ordered = true;
+	cout << "Введите число: \n";
+	cin >> tmp;
+	while (tmp != 0)
+	{
+		cin >> next;
+		if (next != 0 && tmp > next) { ordered = false; }
+		tmp = next;
+	}
+	if (ordered)
+	{
+		cout << "Последовательность упорядочена по возрастанию" << endl;
+	}
+	else
+	{
+		cout << "Последовательность не упорядочена по возрастанию" << endl;
+	}
+	return 0;
+}
