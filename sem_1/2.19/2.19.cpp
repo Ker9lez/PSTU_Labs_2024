29e85b8f08d26d4c3c1978aceb0a3e8776a62a03
Ker9lez <r_n_a_07@mail.ru>
2024-12-22 22:47:29 +0500

    2.19

---
+++ sem_1/2.19/2.19.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	int sum = 0;
+	cout << "Введите число: \n";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 3 == 0) { sum -= i; }
+		else { sum += i; }
+	}
+	cout << sum << endl;
+	return 0;
+}
