13c2865003ac7612c2f47cfac4c19bac2619429b
Ker9lez <r_n_a_07@mail.ru>
2024-12-24 00:14:43 +0500

    recursion

---
+++ sem_1/recursion/recursion.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int Fibonacci(int n) 
+{
+	if (n <= 2) { return 1; }
+	else { return Fibonacci(n - 1) + Fibonacci(n - 2); }
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, ans;
+	cout << "Введите номер числа в ряде Фибоначчи(1, 1, 2, ...): \n";
+	cin >> n;
+	ans = Fibonacci(n);
+	cout << "Данный член ряда Фибоначчи равен - " << ans << endl;
+	return 0;
+}
