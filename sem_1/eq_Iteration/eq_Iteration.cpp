0beb7a28bb020c4542309b05fc07953ae66d01ad
Ker9lez <r_n_a_07@mail.ru>
2024-12-23 22:29:51 +0500

    eq_Iteration

---
+++ sem_1/eq_Iteration/eq_Iteration.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x = 2, xPrev = 0;
+	double eps = 0.000001, lambda = -0.05;
+	while (abs(x - xPrev) > eps)
+	{
+		xPrev = x;
+		x = lambda * (pow(xPrev, 3) - xPrev - 1) + xPrev;
+	}
+	cout << "Корень по методу итераций: " << x << endl;
+	return 0;
+}
