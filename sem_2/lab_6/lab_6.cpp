781e316e3ed0f2385c956476fde1acf86c2e61a5
Ker9lez <r_n_a_07@mail.ru>
2025-04-14 10:30:34 +0500

    lab_6

---
+++ sem_2/lab_6/lab_6.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    char s[256];
+    char processed[256];
+
+    cout << "Введите строку (оканчивается точкой): ";
+    cin.getline(s, 256);
+
+    int j = 0;
+    for (int i = 0; s[i] != '\0' && s[i] != '.'; i++) 
+    {
+        if (s[i] != ' ')
+        {
+            processed[j] = tolower(s[i]);
+            j++;
+        }
+    }
+    processed[j] = '\0';
+
+    bool isPalindrome = true;
+    int len = j;
+    for (int i = 0; i < len / 2; i++)
+    {
+        if (processed[i] != processed[len - 1 - i])
+        {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if (isPalindrome) 
+    {
+        cout << "Строка является палиндромом." << endl;
+    }
+    else 
+    {
+        cout << "Строка НЕ является палиндромом." << endl;
+    }
+
+    return 0;
+}
